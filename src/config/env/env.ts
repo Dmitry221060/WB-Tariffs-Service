c5d2cb68b8028949700d5343ebdf37b36385effe
Dmitry221060 <ya.dima42103ya@yandex.ru>
2025-09-27 11:01:31 +0500

    Fix CI

--- src/config/env/env.ts
+++ src/config/env/env.ts
@@ -36,10 +36,10 @@ const env = envSchema.parse({
     POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
     GOOGLE_API_KEYFILE: process.env.GOOGLE_API_KEYFILE,
     WB_API_KEY: process.env.WB_API_KEY,
-    WORKER_QUERY_INTERVAL: process.env.WORKER_QUERY_INTERVAL ?? 3600000, // 1 hour
-    FORMAT_EXPORT: process.env.FORMAT_EXPORT ?? true,
+    WORKER_QUERY_INTERVAL: process.env.WORKER_QUERY_INTERVAL ?? "3600000", // 1 hour
+    FORMAT_EXPORT: process.env.FORMAT_EXPORT ?? "true",
     NODE_ENV: process.env.NODE_ENV,
-    APP_PORT: process.env.APP_PORT ?? 5000,
+    APP_PORT: process.env.APP_PORT ?? "5000",
 });
 
 export default env;
